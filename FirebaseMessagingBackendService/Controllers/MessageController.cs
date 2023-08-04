d4e06db187d76ba446bc00e557c31cfad4bd70f6
Cedric Gabrang <cedric@xam.com.au>
2023-08-04 14:53:09 +0800

    Added backend service for sending push notifications

---
+++ FirebaseMessagingBackendService/Controllers/MessageController.cs
@@ -0,0 +1,43 @@
+﻿using FirebaseAdmin.Messaging;
+using Microsoft.AspNetCore.Mvc;
+using System.Net;
+
+namespace FirebaseMessagingBackendService.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MessageController : ControllerBase
+    {
+        [HttpPost]
+        public async Task<IActionResult> SendMessageAsync([FromBody] MessageRequest request)
+        {
+            var message = new Message()
+            {
+                Notification = new Notification
+                {
+                    Title = request.Title,
+                    Body = request.Body,
+                },
+                Data = new Dictionary<string, string>()
+                {
+                    ["CustomData"] = "Hello, how are you doing?"
+                },
+                Token = request.DeviceToken
+            };
+
+            var messaging = FirebaseMessaging.DefaultInstance;
+            var result = await messaging.SendAsync(message);
+
+            if (!string.IsNullOrEmpty(result))
+            {
+                // Message was sent successfully
+                return Ok("Message sent successfully!");
+            }
+            else
+            {
+                // There was an error sending the message
+                throw new Exception("Error sending the message.");
+            }
+        }
+    }
+}
---
+++ FirebaseMessagingBackendService/MessageRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace FirebaseMessagingBackendService
+{
+    public class MessageRequest
+    {
+        public string Title { get; set; }
+        public string Body { get; set; }
+        public string DeviceToken { get; set; }
+    }
+}
